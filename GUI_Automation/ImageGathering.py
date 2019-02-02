8ec9b192bbfe2312e26aaa7d7358ffe1738eaca6
Jonathan Stewart <35748678+andrew2165@users.noreply.github.com>
2019-02-01 19:50:10 -0500

    Update ImageGathering.py

--- GUI_Automation/ImageGathering.py
+++ GUI_Automation/ImageGathering.py
@@ -1,3 +1,43 @@
 #! python 3
+import pyautogui, os, sys, time
 
-# TODO: Write the whole script and test
+print('Press Ctrl-C to quit.')
+
+width, height = pyautogui.size()
+
+# TODO: Find images that are needed
+
+lop = []
+
+for file_path in lop:
+    if os.path.exists(file_path) is True:
+        continue
+    else:
+        print(f'File Path {file_path} Provided is Not Valid')
+        sys.exit()
+
+current_mouse = pyautogui.position()
+
+# Start Timelapse with AmScope Open
+
+capture_button = pyautogui.locateOnScreen('image.png')
+
+if capture_button is None:
+    print('Capture_button not found')
+    sys.exit()
+
+pyautogui.click(capture_button, button='left')  # Fix for pixels
+
+start_time_lapse_button = pyautogui.locateOnScreen('image.png')
+
+if start_time_lapse_button is None:
+    print('start_time_lapse_button not found')
+    sys.exit()
+
+pyautogui.click(start_time_lapse_button, button='left')
+
+ok_button = pyautogui.locateOnScreen('image.png')
+
+if ok_button is None:
+    print('ok_button not found')
+    sys.exit()
